28830c045ad68da2c12d00c6c35d2add293b2b50
Jhordy Vargas <59989272+JhordySteven@users.noreply.github.com>
2022-12-16 10:13:04 -0500

    modificación page principal portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from '../app/pages/pages.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
